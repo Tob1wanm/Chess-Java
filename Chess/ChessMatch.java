d9f849b309aedebd199ce4748db856b27c65004d
Tobiwanm <138580387+Tob1wanm@users.noreply.github.com>
2023-09-18 14:30:11 +0000

    BoardException & Defensive Programming

--- Chess/ChessMatch.java
+++ Chess/ChessMatch.java
@@ -27,6 +27,7 @@ public class ChessMatch {
 
     private void initialSetup() {
         board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
+        board.placePiece(new Rook(board, Color.BLACK), new Position(7, 3));
         board.placePiece(new King(board, Color.WHITE), new Position(0, 3));
     }
 }
